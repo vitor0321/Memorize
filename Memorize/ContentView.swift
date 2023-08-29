cc966fa90d1e6443a46b4b1bb8fa0090d6129d4c
Walcker <vitor0321@gmail.com>
2023-08-29 20:30:01 -0300

    Feat: create three cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
+         CardView(isFaceUp: true)
+         CardView()
+         CardView()
          CardView()
         }
         .foregroundColor(.orange)
