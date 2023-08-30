d4c7da0ab39815eb95d65b07ddf3b292aad37d36
Walcker <vitor0321@gmail.com>
2023-08-29 21:43:28 -0300

    array cards with ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,36 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["👻","🦹🏼","🥷🏼","👸🏼"]
+    
     var body: some View {
         HStack{
-         CardView(isFaceUp: true)
-         CardView()
-         CardView()
-         CardView()
+            ForEach(emojis.indices, id: \.self){ index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
     }
 }
 
-    struct CardView: View {
-        var isFaceUp: Bool = false
-        
-            var body: some View {
-                ZStack{
-                    if isFaceUp {
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundColor(.white)
-                        RoundedRectangle(cornerRadius: 12)
-                            .strokeBorder(lineWidth: 2)
-                        Text("👻").font(.largeTitle)
-                    } else {
-                        RoundedRectangle(cornerRadius: 12)
-                    }
-                }
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
-                                
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
